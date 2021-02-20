6dd39951e44566098ffeb7affedb418ae0ff6f8f
Ocelots-etc <catherinedgomez@gmail.com>
2021-02-19 22:26:04 -0600

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,5 +1,14 @@
 class StudentsController < ApplicationController
+  
+  # def new
+  #   @student = Student.create
+  # end
+
   def index
     @students = Student.all
+  end
+
+  def show
+    @student = Student.find(params[:id])
   end
 end
